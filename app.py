0d6945a66898ddc95500a5872df23a4324f07bd0
Dhruv Tailor <dhruv.tailor007@gmail.com>
2025-07-30 23:42:30 -0400

    Added release files

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def predict():
         return render_template('index.html', prediction = prediction, input_text = input_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
